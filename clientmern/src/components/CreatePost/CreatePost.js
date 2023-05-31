ee581cb97d3208477ac7e81ecacd9f3d063c69bb
Dhara_Saikumar <saikumar.dhara@sportsit.net>
2023-05-31 11:29:03 +0530

    creating the create post in desktop

--- clientmern/src/components/CreatePost/CreatePost.js
+++ clientmern/src/components/CreatePost/CreatePost.js
@@ -17,44 +17,42 @@ const CreatePost = () => {
     return (
         <React.Fragment>
             <div className='create-post-Page'>
-                <div className='create-Post-Section'>
-                    <div className='container'>
-                        <div className='create-Post-Header'>
-                            <div className='create-post-title'>CreatePost</div>
-                        </div>
-                        <div className='create-Post-Body'>
-                            <div className='form'>
-                                <div className='form-group'>
-                                    <div className="username-input">
-                                        <input
-                                            type="text"
-                                            placeholder='title'
-                                            value={title}
-                                            onChange={(e) => setTitle(e.target.value)} />
-                                    </div>
-                                    <div className="username-input">
-                                        <input
-                                            type="text"
-                                            placeholder='Summary'
-                                            value={summary}
-                                            onChange={(e) => setSummary(e.target.value)} />
-                                    </div>
-                                    <div className="username-input">
-                                        <input
-                                            type="file"
-                                            placeholder='Image'
-                                            value={image}
-                                            onChange={(e) => setImage(e.target.value)} />
-                                    </div>
-                                    <div className="username-input">
-                                        <input
-                                            type="number"
-                                            placeholder='Price...'
-                                            value={price}
-                                            onChange={(e) => setPrice(e.target.value)} />
-                                    </div>
-                                    <button className="register-btn" onClick={handleAddItem}>Add Item </button>
+                <div className='container'>
+                    <div className='create-Post-Header'>
+                        <div className='create-post-title'>CreatePost</div>
+                    </div>
+                    <div className='create-Post-Body'>
+                        <div className='form'>
+                            <div className='form-group'>
+                                <div className="username-input">
+                                    <input
+                                        type="text"
+                                        placeholder='title'
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)} />
                                 </div>
+                                <div className="username-input">
+                                    <input
+                                        type="text"
+                                        placeholder='Summary'
+                                        value={summary}
+                                        onChange={(e) => setSummary(e.target.value)} />
+                                </div>
+                                <div className="username-input">
+                                    <input
+                                        type="file"
+                                        placeholder='Image'
+                                        value={image}
+                                        onChange={(e) => setImage(e.target.value)} />
+                                </div>
+                                <div className="username-input">
+                                    <input
+                                        type="number"
+                                        placeholder='Price...'
+                                        value={price}
+                                        onChange={(e) => setPrice(e.target.value)} />
+                                </div>
+                                <button className="register-btn" onClick={handleAddItem}>Add Item </button>
                             </div>
                         </div>
                     </div>
