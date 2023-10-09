1d559a7d50c15e0b0421238e5191437f820b32e9
saikumardhara <saikumar.dhara@sportsit.net>
2023-10-09 21:56:10 +0530

    worked on home page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mern_stack
 full stack project
+// to run the node backend project 
+use  => node server.js
--- clientmern/src/components/Banners/BlogBanner.js
+++ clientmern/src/components/Banners/BlogBanner.js
@@ -6,19 +6,25 @@ const BlogBanner = () => {
     const splideRef = useRef(null);
 
     useEffect(() => {
-      const interval = setInterval(() => {
-        splideRef.current.go('>');
-      }, 3000); // Adjust the interval (in milliseconds) between slide transitions
-  
-      return () => {
-        clearInterval(interval);
-      };
+        const interval = setInterval(() => {
+            splideRef.current.go('>');
+        }, 3000); // Adjust the interval (in milliseconds) between slide transitions
+
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
     return (
         <React.Fragment>
             <div className='Blog-Banner-Main'>
                 <div className='container'>
                     <div className='banner-content'>
+                        <div className='banner-Description'>
+                            <p className='title'>Technology</p>
+                            <h2>Creative Space</h2>
+                            <p className='sub-title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
                         <Splide hasTrack={false}
                             options={{
                                 perPage: 1,
@@ -29,16 +35,20 @@ const BlogBanner = () => {
                                 interval: 3000,
                             }}
                             ref={splideRef}
-                            >
+                        >
                             <SplideTrack>
                                 <SplideSlide id='slide1'>
-                                    <img src="https://img.freepik.com/free-photo/portrait-young-asian-woman-isolated-blue-studio-space_155003-12397.jpg?w=1060&t=st=1685602063~exp=1685602663~hmac=fa4c17971547c5b051ae23493d5c6df5784e8f342fe1623cf1a5285a502c2020" alt="Image 1" />
+                                    <img src="https://mobirise.com/extensions/coworkingamp/coworking_space/assets/images/02.jpg" alt="Image 1" />
                                 </SplideSlide>
                                 <SplideSlide id='slide2'>
-                                    <img src="https://img.freepik.com/free-photo/confident-macho-man-put-sunglasses-looking-cool-sassy-standing-blue-background_1258-153254.jpg?w=1060&t=st=1685601932~exp=1685602532~hmac=31a7efe6d5085f624eefe0237485e0bff380cd04cc193f75e90aa9eb32e0c59e" alt="Image 2" />
+                                    <img src="https://mobirise.com/extensions/coworkingamp/coworking_space/assets/images/07.jpg" alt="Image 2" />
+                                </SplideSlide>
+                                <SplideSlide id='slide3'>
+                                    <img src="https://mobirise.com/extensions/coworkingamp/coworking_space/assets/images/10.jpg" alt="Image 2" />
                                 </SplideSlide>
                             </SplideTrack>
                         </Splide>
+
                     </div>
                 </div>
             </div>
