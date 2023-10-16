7af5848d89f8b88cdabf92ab6182a1e77423d4da
Dhara_Saikumar <saikumar.dhara@sportsit.net>
2023-10-16 19:01:10 +0530

    worked on cart

--- clientmern/src/components/Common/CartPage.jsx
+++ clientmern/src/components/Common/CartPage.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CartPage = () => {
+    const [count, setCount] = useState(0)
     const data = {
         "imageUrl": "https://rukminim1.flixcart.com/image/612/612/kfmv9u80-0/jean/e/9/e/36-hljn000670-highlander-original-imafwfj6zhxw39wg.jpeg?q=70",
         "brand": "HIGHLANDER",
@@ -45,22 +48,60 @@ const CartPage = () => {
                                 <h3 className='product-description'>
                                     {data?.title}
                                 </h3>
+                                <div className='review-status'>
+                                    Review
+                                </div>
                             </div>
                             <div className='product-price-details'>
-                                <div className='product-description'>
+                                <h4 >
                                     {"$ " + data?.price}
+                                </h4>
+                                <div className='product-description' >
+                                    <div className='content-txt'>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                        laboris nisi.
+                                    </div>
+                                    <div className='content-txt'>
+                                        AVAILABILITY : {" IN STOCK "}
+                                    </div>
+                                    <div className='content-txt'>
+                                        CATEGORIES : {" JEANS "}
+                                    </div>
+                                    <div className='color-content'>
+                                        <span className='colour-txt'>Colours : </span>
+                                        <div className='color-button'><span className='colour-btn'></span></div>
+                                        <div className='color-button'><span className='colour-btn'></span></div>
+
+                                    </div>
                                 </div>
                             </div>
                             <div className='product-cart-details'>
                                 <div className='product-description'>
-                                    <button className='btn button2'> Add to Cart</button>
+                                    <button className='btn plus-btn ' onClick={() => setCount(count - 1)}>
+                                        -
+                                    </button>
+                                    <button className='btn plus-btn'> {count}</button>
+                                    <button className='btn plus-btn' onClick={() => setCount(count + 1)}>
+                                        +
+                                    </button>
                                 </div>
                                 <div className='product-description'>
-                                    <button className='btn button2'> Buy now</button>
+                                    <button className=' like-btn button2'>
+                                        <FontAwesomeIcon icon={faHeart} />
+                                    </button>
+                                </div>
+                                <div className='product-description'>
+                                    <button className=' button2'> Add to Cart</button>
+                                </div>
+                                <div className='product-description'>
+                                    <button className=' button2'> Buy now</button>
                                 </div>
                             </div>
                         </div>
                         <div className='cart-footer'>
+
                         </div>
                     </div>
                 </div>
